4fb9e73426209e646b8ef90b750fb9ada521a90f
pluto <linuxfingers@pm.me>
2020-03-13 09:31:27 -0400

    commented out gapless, fixed transparency in compton

--- dwm/config.h
+++ dwm/config.h
@@ -47,11 +47,11 @@ static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] *
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
-#include "gaplessgrid.c"
+//#include "gaplessgrid.c"
 static const Layout layouts[] = {
 	/* symbol     	arrange function */
 	
-	{ "\u26b7",	gaplessgrid},
+//	{ "\u26b7",	gaplessgrid},
 	{ "\u2685",      	tile },    /* first entry is default */
 	{ "\u2680",      	NULL },    /* no layout function means floating behavior */
 	{ "\u2699",      	monocle },
@@ -76,7 +76,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[0] } },
+//	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[0] } }, 
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
